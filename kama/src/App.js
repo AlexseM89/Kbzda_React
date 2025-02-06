b718871be2e09c58058d44888fc98f221bbd8f6c
Aleksei Melenchuk <melenchuk89@mail.ru>
2025-02-06 16:38:08 +0300

    Lesson09 разнесли компоненты по отдельным файлам .js

--- kama/src/App.js
+++ kama/src/App.js
@@ -1,34 +1,13 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Technologies from "./Technologies";
+import Header from "./Header";
 
 const App = () => {
     return (
         <div>
-            <div>
-                <Header />
-                <Technologies />
-            </div>
-        </div>
-    );
-};
-const Header = () => {
-    return (
-        <div>
-            <a href="#s">Home</a>
-            <a href="#s">News Feed</a>
-            <a href="#s">Messages</a>
-        </div>
-    );
-};
-const Technologies = () => {
-    return (
-        <div>
-            <ul>
-                <li>cssT</li>
-                <li>htmlT</li>
-                <li>jsT</li>
-                <li>ReactT</li>
-            </ul>
+            <Header />
+            <Technologies />
         </div>
     );
 };
